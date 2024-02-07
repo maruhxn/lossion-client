26847d65d41945a5ee1d798640c10ed1dc689a48
maruhxn_ <maruhan1016@gmail.com>
2024-02-07 18:28:56 +0900

    Feat: Add Basic Settings

---
+++ src/components/providers.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60,
+        retry: 1,
+      },
+      mutations: {
+        retry: 1,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default Providers;
